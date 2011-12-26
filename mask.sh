b0f074a5b323da1ba4c11af0769a3bdec2aff714
Maher Gamal <mahergamal@gmail.com>
2011-12-26 23:14:40 +0200

    Reorganized the packaging, plus made the input taken from System.in rather than the standard args.

--- mask.sh
+++ mask.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn -q -f luhn-algorithm/pom.xml clean compile exec:java -Dexec.mainClass=mahergamal.algorithms.luhn.ConsoleRunner -Dexec.arguments="$*"
+mvn -q -f luhn-algorithm/pom.xml clean compile exec:java -Dexec.mainClass=mahergamal.logging.filters.LuhnLogFilter -Dexec.arguments="$*"
